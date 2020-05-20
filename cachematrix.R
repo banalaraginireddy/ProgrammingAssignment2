28f0c81c2e8a7e99d02196b1668f39cae53dd293
banalaraginireddy <35792053+banalaraginireddy@users.noreply.github.com>
2020-05-20 21:48:12 +0530

    modified version from r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## install matlib library to use inv like functions
+## this function has four functions like setinv getinv to retrieve inverse functions
+## whereas get and set to retrieve the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
+  minv<- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() minv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+ }
+## here it checks whether it is already cached or not if it is not chached we get the inverse matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- inv(data, ...)
+  x$setinv(m)
+  m
+  }
